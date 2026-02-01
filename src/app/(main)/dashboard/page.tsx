e3d6bfbb7c802ddc93a3b15ca054de20a34aff9b
ReactiveX22 <akibreactive@gmail.com>
2026-02-01 21:58:50 +0600

    feat: add admin dashboard

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { RoleGuard } from '@/components/common/role-guard';
+import { AdminDashboard } from '@/components/dashboard/admin-dashboard';
 import { StudentDashboard } from '@/components/dashboard/student-dashboard';
 import { TeacherDashboard } from '@/components/dashboard/teacher-dashboard';
 import { Role } from '@/types/auth';
@@ -9,8 +10,11 @@ export default function DashboardPage() {
       <RoleGuard allowedRoles={[Role.Student]}>
         <StudentDashboard />
       </RoleGuard>
-      <RoleGuard allowedRoles={[Role.Instructor, Role.Admin]}>
+      <RoleGuard allowedRoles={[Role.Instructor]}>
         <TeacherDashboard />
+      </RoleGuard>
+      <RoleGuard allowedRoles={[Role.Admin]}>
+        <AdminDashboard />
       </RoleGuard>
     </>
   );
